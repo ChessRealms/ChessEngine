b71afd8610c55b02e5d0b4aa809643325bddb1ca
denyrt <denisyrt02@gmail.com>
2024-05-27 21:02:58 +0300

    Removed experimental benchmarks.

--- src/ChessRealms.ChessEngine2.Benchmark/Program.cs
+++ src/ChessRealms.ChessEngine2.Benchmark/Program.cs
@@ -1,8 +1,6 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.InProcess.Emit;
-using ChessRealms.ChessEngine2.Benchmark.MoveGenerations;
 
 Console.WriteLine("Hello, Benckmarks!");
 Console.WriteLine();
@@ -12,15 +10,3 @@ var config = DefaultConfig.Instance
          .MediumRun
          .WithLaunchCount(1)
          .WithToolchain(InProcessEmitToolchain.DontLogOutput));
-
-BenchmarkRunner.Run<PawnMoveGenerationBenchmarks>(config);
-BenchmarkRunner.Run<KnightMoveGenerationBenckmarks>(config);
-BenchmarkRunner.Run<BishopMoveGenerationBenchmarks>(config);
-BenchmarkRunner.Run<RookMoveGenerationBenchmarks>(config);
-BenchmarkRunner.Run<QueenMoveGenerationBenchmarks>(config);
-BenchmarkRunner.Run<KingMoveGenerationBenchmarks>(config);
-
-BenchmarkRunner.Run<CastlingMoveGenerationBenchmarks>(config);
-BenchmarkRunner.Run<IsSquareAttackedBenchmarks>(config);
-
-BenchmarkRunner.Run<AllMoveGenerationBenchmarks>(config);
